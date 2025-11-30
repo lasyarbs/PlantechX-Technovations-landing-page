a668344dd2ffa43e6e00a262cbc3cb80bbe9d24b
Lasya RBS <146111770+lasyarbs@users.noreply.github.com>
2025-11-30 19:35:16 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,12 @@ function App() {
   return (
     <DemoModalProvider>
       <div className="min-h-screen bg-black">
+
+        {/* STEP 1: Start with only Header */}
         <Header />
+
+        {/* UNCOMMENT ONE BY ONE TO FIND WHICH COMPONENT BREAKS */}
+        {/*
         <Hero />
         <About />
         <Features />
@@ -25,6 +30,8 @@ function App() {
         <CTA />
         <Footer />
         <DemoModal />
+        */}
+
       </div>
     </DemoModalProvider>
   );
